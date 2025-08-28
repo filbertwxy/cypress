8ff279d8a3d17fd8932e979ad9e5dcae30cddeb8
filbert <filbertwijaya161616@gmail.com>
2025-08-28 22:26:53 +0700

    coba kedua kali

--- cypress/e2e/Baskit/apiTest.cy.js
+++ cypress/e2e/Baskit/apiTest.cy.js
@@ -1,32 +1,23 @@
-
-describe("API Testing Example", () => {
-
-
-
+describe('API Testing Example', () => {
   it('Login to Baskit', () => {
-
     cy.fixture('data').then((data) => {
-        const requestBody = {
-            username: data.username,
-            password: data.password
-        }
-    
+      const requestBody = {
+        username: data.username,
+        password: data.password,
+      }
 
-    cy.request({
-     method: 'POST',
-     url: 'https://api-beta.baskit.app/v2/auth',
-    
-    body:requestBody
+      cy.request({
+        method: 'POST',
+        url: 'https://api-beta.baskit.app/v2/auth',
+
+        body: requestBody,
+      })
     })
-  })
-  
+
   .then((response) => {
-     expect(response.status).to.eq(200)
-      cy.log(JSON.stringify(response.body))
+    expect(response.status).to.eq(200)
+    cy.log(JSON.stringify(response.body))
     //   expect(response.body.username).to.eq('supercompany.proofs748@passinbox.com')
-
-    })
-
   })
-
+  })
 })
